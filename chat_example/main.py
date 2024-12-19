72e0a0b2d036a59ade6abd943cb2865f5cd431b3
Gil <ghopecs@gmail.com>
2024-12-19 09:31:59 -0500

    add mem

--- chat_example/main.py
+++ chat_example/main.py
@@ -3,14 +3,18 @@ from langchain_community.chat_models import ChatOpenAI
 # from langchain.chains import LLMChain
 from langchain.chains import LLMChain
 from langchain.prompts import MessagesPlaceholder, HumanMessagePromptTemplate, ChatPromptTemplate
-from langchain.memory import ConversationBufferMemory
+from langchain.memory import ConversationBufferMemory, FileChatMessageHistory
 from dotenv import load_dotenv
 
 load_dotenv()   
 
 chat = ChatOpenAI()
 
-memory = ConversationBufferMemory(memory_key="messages", return_messages=True)
+memory = ConversationBufferMemory(
+    chat_memory=FileChatMessageHistory("messages.json"),
+    memory_key="messages", 
+    return_messages=True
+)
 
 prompt = ChatPromptTemplate(
     input_variables=["content", "messages"],
