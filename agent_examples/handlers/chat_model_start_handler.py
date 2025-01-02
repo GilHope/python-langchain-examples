051ac1a6215993927ea2bf7e4d261162c3696a4b
Gil <ghopecs@gmail.com>
2025-01-02 15:54:13 -0500

    first boxen test

--- agent_examples/handlers/chat_model_start_handler.py
+++ agent_examples/handlers/chat_model_start_handler.py
@@ -1,4 +1,10 @@
 from langchain.callbacks.base import BaseCallbackHandler
+from pyboxen import boxen
+
+def boxen_print(*args, **kwargs):
+    print(boxen(*args, **kwargs))
+
+boxen_print("TEXT HERE!!", title="Human", color="Yellow")
 
 class ChatModelStartHandler(BaseCallbackHandler):
     def on_chat_model_start(self, serialized, messages, **kwargs):
