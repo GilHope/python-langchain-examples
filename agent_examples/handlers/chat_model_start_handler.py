63bd3701b1c36782bcd391a13c6463f7253fb052
Gil <ghopecs@gmail.com>
2025-01-03 09:50:13 -0500

    add additional elif types

--- agent_examples/handlers/chat_model_start_handler.py
+++ agent_examples/handlers/chat_model_start_handler.py
@@ -11,3 +11,23 @@ class ChatModelStartHandler(BaseCallbackHandler):
         for message in messages[0]:
             if message.type == "system":
                 boxen_print(message.content, title=message.type, color="yellow")
+
+            elif message.type == "human":
+                boxen_print(message.content, title=message.type, color="green")
+
+            elif message.type == "ai" and "function_call" in message.additional_kwargs:
+                call = message.additional_kwargs["function_call"]
+                boxen_print(
+                f"Running tool {call['name']} with args {call['arguments']}",
+                title=message.type,
+                color="cyan"
+           )
+                
+            elif message.type == "ai":
+                boxen_print(message.content, title=message.type, color="blue")
+
+            elif message.type == "function":
+                boxen_print(message.content, title=message.type, color="purple")
+
+            else:
+                boxen_print(message.content, title=message.type)
