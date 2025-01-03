6e6a87dcd05cb58b79da87f98b38a207c5cd765f
Gil <ghopecs@gmail.com>
2025-01-03 09:39:11 -0500

    update

--- agent_examples/handlers/chat_model_start_handler.py
+++ agent_examples/handlers/chat_model_start_handler.py
@@ -4,8 +4,10 @@ from pyboxen import boxen
 def boxen_print(*args, **kwargs):
     print(boxen(*args, **kwargs))
 
-boxen_print("TEXT HERE!!", title="Human", color="Yellow")
-
 class ChatModelStartHandler(BaseCallbackHandler):
     def on_chat_model_start(self, serialized, messages, **kwargs):
-        print(messages)
+        print("\n\n============ Sending Messages ============\n\n")
+
+        for message in messages[0]:
+            if message.type == "system":
+                boxen_print(message.content, title=message.type, color="yellow")
